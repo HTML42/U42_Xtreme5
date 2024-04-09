5ea2eeb3223c462f40ed2f7328b43579f447538f
mark <external_github@power-of-m.de>
2024-04-09 22:13:07 +0200

    structure add

---
+++ install/_structure/php/objects/css/xtreme.php
@@ -0,0 +1,13 @@
+<?php
+
+$files = array(
+    File::i('css/xtreme/mixins.less')->path,
+    File::i('css/xtreme/basics.less')->path,
+    File::i('css/xtreme/header.less')->path,
+);
+$Css = new Css($files, App::config('xcss', []));
+
+App::$mime = 'text/css';
+App::$encoding = 'UTF-8';
+
+Response::deliver($Css->code);
--- install/_structure/php/objects/js/xtreme.php
+++ install/_structure/php/objects/js/xtreme.php
@@ -2,13 +2,13 @@
 
 $files = array(
     'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js',
-    File::i('js/classes/app.class.js'),
-    File::i('js/classes/controller.class.js'),
-    File::i('js/classes/page.class.js'),
-    File::i('js/classes/router.class.js'),
-    File::i('js/classes/template.class.js'),
-    File::i('js/classes/translation.class.js'),
-    File::i('js/functions.js'),
+    File::i('js/classes/app.class.js')->path,
+    File::i('js/classes/controller.class.js')->path,
+    File::i('js/classes/page.class.js')->path,
+    File::i('js/classes/router.class.js')->path,
+    File::i('js/classes/template.class.js')->path,
+    File::i('js/classes/translation.class.js')->path,
+    File::i('js/functions.js')->path,
 );
 $files = array_merge($files, glob(DIR_PROJECT . 'js/controller/*.controller.js'));
 $files[] = File::i('js/bootstrap.js');
