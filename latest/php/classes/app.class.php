aacddd82ca5ea8149c1c0f31583b3a720025b7bd
mark <external_github@power-of-m.de>
2024-04-14 15:30:19 +0200

    App

--- latest/php/classes/app.class.php
+++ latest/php/classes/app.class.php
@@ -9,6 +9,7 @@ class App
     public static $action = null;
     
     public static $config = [];
+    public static $images = [];
     
     public static function config($key, $default = null) {
         return isset(self::$config[$key]) && !is_null(self::$config[$key]) ? self::$config[$key] : $default;
