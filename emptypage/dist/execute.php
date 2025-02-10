e077a7e3b80ddb1405af53e1baf576ef3216f5ac
MarkPaspirgilis <external_github@power-of-m.de>
2025-02-10 12:20:17 +0100

    Update execute.php

--- emptypage/dist/execute.php
+++ emptypage/dist/execute.php
@@ -4,6 +4,8 @@ if(is_file('/var/www/U42_Xtreme5/latest/x5_start.php')) {
     include '/var/www/U42_Xtreme5/latest/x5_start.php';
 } else if(is_file('D:/Webserver/U42/Intern/Xtreme5/latest/x5_start.php')) {
     include 'D:/Webserver/U42/Intern/Xtreme5/latest/x5_start.php';
+} else if(is_file('/Users/mark/Development/mp/x5/latest/x5_start.php')) {
+    include '/Users/mark/Development/mp/x5/latest/x5_start.php';
 } else {
     include '../vendor/u42/x5/latest/x5_start.php';
 }
