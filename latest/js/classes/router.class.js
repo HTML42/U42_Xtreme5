b6a4153f1cf889c302801c7e228d0f46def09ffc
MarkPaspirgilis <external_github@power-of-m.de>
2026-02-15 23:29:33 +0100

    Fix router hash parsing and expose last route id

--- latest/js/classes/router.class.js
+++ latest/js/classes/router.class.js
@@ -1,19 +1,26 @@
 class RouterClass {
     route() {
+        window.last_route_id = null;
         let location_hash = location.hash;
         if(location_hash.length <= 2) {
             return null;
         }
         location_hash = location_hash.trim();
         let route_match = location_hash.match(/\#\!([^\/]+)\/*([^\/]*)\/*([^\/]*)\/*([^\/]*)\/*([^\/]*)/i);
+        if(!route_match || route_match.length < 2) {
+            return null;
+        }
         let route_path = ['index', 'index', null, null, null];
         for(let i = 1 ; i <= 5 ; i++) {
-            if((route_match[i] && route_match[i].length) || typeof route_match[i] == 'number' || route_match[i] === '0' || route_match[i] === 'false' || route_match[i] === 'null') {
+            if(route_match[i] !== undefined && route_match[i] !== '') {
                 route_path[i - 1] = route_match[i];
                 if(route_path[i - 1] == parseInt(route_path[i - 1])) {
                     route_path[i - 1] = parseInt(route_path[i - 1]);
                 }
             }
+        }
+        if(typeof route_path[2] !== 'undefined' && route_path[2] !== null) {
+            window.last_route_id = route_path[2];
         }
         return route_path;
     }
